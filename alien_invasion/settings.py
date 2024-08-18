5d296f8c1fc5149a24ec646946a0fa4901b5fb7b
yuki <yukichaniu@gmail.com>
2024-08-18 15:05:44 -0400

    small screen

--- alien_invasion/settings.py
+++ alien_invasion/settings.py
@@ -4,8 +4,9 @@ class Settings():
     def __init__(self):
         """Initialize the game settings"""
         #Screen settings
-        self.screen_width = 1500
-        self.screen_height = 800
+        self.screen_width = 10
+        self.screen_height = 1000
         self.bg_color = (230, 230, 230)
         self.ship_speed_factor = 2
 
+t
